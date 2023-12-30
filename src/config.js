71006ff742a1c890bfa5e86cdc6b86e4516029e9
Dick Hardt <dick.hardt@gmail.com>
2023-12-30 14:11:43 -0800

    fix issuer string

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ const { env } = process
 
 export const PORT = env.PORT || 3333
 export const IP = env.IP || '127.0.0.1'
-export const ISSUER = env.ISSUER || `http:${IP}:${PORT}`
+export const ISSUER = env.ISSUER || `http://${IP}:${PORT}`
 
 if ('https://issuer.hello.coop' === ISSUER.toLowerCase().replace(/\/$/, "")) {
     throw new Error('ISSUER must not be https://issuer.hello.coop')
